8ab461fb7d2f8ad8e2e8625caec1e3d729830ec7
eduAguilar96 <eduxx77@gmail.com>
2017-04-27 14:45:19 -0500

    ShowMenu() methos and blank switch added

--- main.cpp
+++ main.cpp
@@ -4,6 +4,54 @@ using namespace std;
 
 #include "Hora.h"
 
+void showMenu(){
+    cout << "-------------------MENU-------------------" <<  endl;
+    cout << endl;
+    cout << "1. Consultar la lista de servicios" << endl;
+    cout << "2. Consultar la lista de reservaciones" << endl;
+    cout << "3. Consultar las reservaciones de un servicio dado" << endl;
+    cout << "4. Consultar las reservaciones de una hora especsifica" << endl;
+    cout << "5. Hacer una reservacion" << endl;
+    cout << "6. Terminar" << endl;
+    cout << endl;
+    cout << "Porfavor ingrese el numero de la opcion que desee : ";
+}
+
 int main() {
+    
+    
+    showMenu();
+    int iOpt = 0;
+    
+    while(iOpt != 6 ){
+        
+        showMenu();
+        cin >> iOpt;
+        
+        switch (iOpt) {
+            case 1:
+                break;
+                
+            case 2:
+                break;
+                
+            case 3:
+                break;
+                
+            case 4:
+                break;
+                
+            case 5:
+                break;
+                
+            case 6:
+                cout << "Saliendo de la applicacion" << endl;
+                break;
+                
+            default:
+                cout << "Esa opcion es invalida" << endl;
+                break;
+        }
+    }
     return 0;
 }
